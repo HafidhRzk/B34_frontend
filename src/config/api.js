0f9326658530716f7e1948701a7d16fb693a0d5a
Hafidh Rizky <hafidhrzk@gmail.com>
2022-06-08 01:01:17 +0700

    third commit

--- src/config/api.js
+++ src/config/api.js
@@ -7,18 +7,18 @@ export const API = () => {
     'https://b34-backend.herokuapp.com/api/v1' ||
     'https://localhost:5000/api/v1';
 
-  const executeAPI = async (endpoint, config) => {
-    const response = await fetch(baseUrl + endpoint, config);
-    const data = await response.json();
-    return data;
-  };
+  // const executeAPI = async (endpoint, config) => {
+  //   const response = await fetch(baseUrl + endpoint, config);
+  //   const data = await response.json();
+  //   return data;
+  // };
 
-  return {
-    get: executeAPI,
-    post: executeAPI,
-    patch: executeAPI,
-    delete: executeAPI,
-  };
+  // return {
+  //   get: executeAPI,
+  //   post: executeAPI,
+  //   patch: executeAPI,
+  //   delete: executeAPI,
+  // };
 };
 
 // Set Authorization Token Header
