85c51ae66f4085a387873ec1f764e07cdc327945
Hafidh Rizky <hafidhrzk@gmail.com>
2022-06-08 01:28:56 +0700

    fifth commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,24 +1,30 @@
 import axios from 'axios';
 
 // Create base URL API
-export const API = () => {
-  baseUrl: process.env.REACT_APP_SERVER_URL ||
-    'https://b34-backend.herokuapp.com/api/v1' ||
-    'https://localhost:5000/api/v1';
+export const API = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL ||
+      'https://b34-backend.herokuapp.com/api/v1' ||
+      'https://localhost:5000/api/v1',
+});
 
-  // const executeAPI = async (endpoint, config) => {
-  //   const response = await fetch(baseUrl + endpoint, config);
-  //   const data = await response.json();
-  //   return data;
-  // };
+// export const API = () => {
+//   const baseUrl = process.env.REACT_APP_SERVER_URL ||
+//     'https://b34-backend.herokuapp.com/api/v1' ||
+//     'https://localhost:5000/api/v1';
 
-  // return {
-  //   get: executeAPI,
-  //   post: executeAPI,
-  //   patch: executeAPI,
-  //   delete: executeAPI,
-  // };
-};
+//   const executeAPI = async (endpoint, config) => {
+//     const response = await fetch(baseUrl + endpoint, config);
+//     const data = await response.json();
+//     return data;
+//   };
+
+//   return {
+//     get: executeAPI,
+//     post: executeAPI,
+//     patch: executeAPI,
+//     delete: executeAPI,
+//   };
+// };
 
 // Set Authorization Token Header
 export const setAuthToken = (token) => {
